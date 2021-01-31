661d1e887db3fa127e5711f631d5bc335ca615b7
Debaditya Malakar <63513685+IronManCool001@users.noreply.github.com>
2021-01-31 12:18:51 +0530

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -3,6 +3,7 @@ import pathlib
 from core import *
 lang_name = sys.argv[1]
 file_name = sys.argv[2]
+#if sys.argv[0] != "cy"
 if lang_name == "cy":
     file = pathlib.Path(file_name)
     if file.exists ():
